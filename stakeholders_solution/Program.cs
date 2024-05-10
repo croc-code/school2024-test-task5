3a1897af7e6f443ec1b61f71769ec11eb84f9573
TSRSE <26186238+TSRSE@users.noreply.github.com>
2024-05-11 02:50:59 +0300

    [~] Вынес часть логики в отдельный метод R: Нет возможности переиспользовать данную логику в ходе дальнейшего выполнения программы
    FB: Вынес логику чтения из файла в отдельный асинхронный метод,
    который принимает в себя словарь, который нужно изменить,
    путь к файлу для считывания, индекс для записи в
    координаты и возвращает новый словарь <string, double[]>

--- stakeholders_solution/Program.cs
+++ stakeholders_solution/Program.cs
@@ -12,34 +12,49 @@ internal class Program
 
     // 
     string pathToInterestFile = Path.Combine(AppContext.BaseDirectory, @"..\..\..\..\StakeholdersData\interest.txt");
+    string pathToInfluenceFile = Path.Combine(AppContext.BaseDirectory, @"..\..\..\..\StakeholdersData\influence.txt");
 
+    StakeholdersMap = await GetUpdatedStakeholdersMapFromFileAsync(StakeholdersMap, pathToInterestFile, 0);
+    StakeholdersMap = await GetUpdatedStakeholdersMapFromFileAsync(StakeholdersMap, pathToInfluenceFile, 1);
+    
+    for (int i = 0; i < StakeholdersMap.Keys.ToArray().Length; i++)
+    {
+      // Debug console
+      Console.WriteLine();
+      Console.WriteLine(StakeholdersMap.Keys.ToArray()[i] + " => " + StakeholdersMap[StakeholdersMap.Keys.ToArray()[i]][0] + " : " + StakeholdersMap[StakeholdersMap.Keys.ToArray()[i]][1]);
+      // 
+    }
+  }
+
+  private static async Task<Dictionary<string, double[]>> GetUpdatedStakeholdersMapFromFileAsync(Dictionary<string, double[]> currentStakeholdersMap, string path, int coordinateIndex){
     // Читаем асинхронно все линии из файла, 
     // чтобы наше приложение не зависало, пока считывает файл
-    string[] LinesFromInterest = await File.ReadAllLinesAsync(pathToInterestFile);
+    string[] LinesFromFile = await File.ReadAllLinesAsync(path);
 
     // Первую линию сплиттим по символу '|', получаем массив стейкхолдеров
-    string[] StakeHoldersFromHeader = LinesFromInterest[0].Split('|');
+    string[] StakeHoldersFromHeader = LinesFromFile[0].Split('|');
 
     // Добавляем имена стейкхолдеров как значения в наш словарь
     for (int i = 0; i < StakeHoldersFromHeader.Length; i++)
     {
-      if (!StakeholdersMap.ContainsKey(StakeHoldersFromHeader[i]))
+      StakeHoldersFromHeader[i] = StakeHoldersFromHeader[i].TrimStart().TrimEnd();
+      if (!currentStakeholdersMap.ContainsKey(StakeHoldersFromHeader[i]))
       {
-        StakeholdersMap.Add(StakeHoldersFromHeader[i], new double[2]);
+        currentStakeholdersMap.Add(StakeHoldersFromHeader[i], new double[2]);
       }
     }
 
     // Присваеваем значение интереса ( X ) к ключу ( Стейкхолдеру )
-    for (int i = 1; i < LinesFromInterest.Length; i++)
+    for (int i = 1; i < LinesFromFile.Length; i++)
     {
-      double currentLineSumm = LinesFromInterest[i].Replace(".", ",").Split(" ").Sum(data => double.TryParse(data, out double number) ? number : 0);
-      StakeholdersMap[StakeHoldersFromHeader[i - 1]][0] = currentLineSumm;
+      double currentLineSumm = LinesFromFile[i].Replace(".", ",").Split(" ").Sum(data => double.TryParse(data, out double number) ? number : 0);
+      currentStakeholdersMap[StakeHoldersFromHeader[i - 1]][coordinateIndex] = currentLineSumm;
 
       // Debug console
-      Console.WriteLine(LinesFromInterest[i]);
-      Console.WriteLine(StakeholdersMap.Keys.ToArray()[i - 1] + ":" + StakeholdersMap[StakeHoldersFromHeader[i - 1]][0]);
+      Console.WriteLine(LinesFromFile[i]);
+      Console.WriteLine(currentStakeholdersMap.Keys.ToArray()[i - 1] + ":" + currentStakeholdersMap[StakeHoldersFromHeader[i - 1]][coordinateIndex]);
       // 
     }
-
+    return currentStakeholdersMap;
   }
 }

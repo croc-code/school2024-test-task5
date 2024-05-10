31cd6b15041a634fbadcab15665eaa213ec2e1ce
TSRSE <26186238+TSRSE@users.noreply.github.com>
2024-05-11 02:21:21 +0300

    [+] Добавил базовую идею реализации задачи R: Нужно было придумать, как считывать информацию из файла и хранить его в связке {Stakeholder <-> Coordinates[x,y]}
    FB: Использовал Dictionary <string, double[]> для связки,
    LINQ для более приятного формата получения
    суммы из массива строк (string[].sum)

--- stakeholders_solution/Program.cs
+++ stakeholders_solution/Program.cs
@@ -1,7 +1,45 @@
-﻿internal class Program
+﻿using System.Collections;
+using System.Collections.Generic;
+
+internal class Program
 {
-    private static void Main(string[] args)
+  private static async Task Main(string[] args)
+  {
+    // Словарь для хранения связки 
+    // Стейкхолдер - Координаты
+    // Stakeholder - [x,y]
+    Dictionary<string, double[]> StakeholdersMap = new Dictionary<string, double[]>();
+
+    // 
+    string pathToInterestFile = Path.Combine(AppContext.BaseDirectory, @"..\..\..\..\StakeholdersData\interest.txt");
+
+    // Читаем асинхронно все линии из файла, 
+    // чтобы наше приложение не зависало, пока считывает файл
+    string[] LinesFromInterest = await File.ReadAllLinesAsync(pathToInterestFile);
+
+    // Первую линию сплиттим по символу '|', получаем массив стейкхолдеров
+    string[] StakeHoldersFromHeader = LinesFromInterest[0].Split('|');
+
+    // Добавляем имена стейкхолдеров как значения в наш словарь
+    for (int i = 0; i < StakeHoldersFromHeader.Length; i++)
     {
-        Console.WriteLine("Hello, World!");
+      if (!StakeholdersMap.ContainsKey(StakeHoldersFromHeader[i]))
+      {
+        StakeholdersMap.Add(StakeHoldersFromHeader[i], new double[2]);
+      }
     }
+
+    // Присваеваем значение интереса ( X ) к ключу ( Стейкхолдеру )
+    for (int i = 1; i < LinesFromInterest.Length; i++)
+    {
+      double currentLineSumm = LinesFromInterest[i].Replace(".", ",").Split(" ").Sum(data => double.TryParse(data, out double number) ? number : 0);
+      StakeholdersMap[StakeHoldersFromHeader[i - 1]][0] = currentLineSumm;
+
+      // Debug console
+      Console.WriteLine(LinesFromInterest[i]);
+      Console.WriteLine(StakeholdersMap.Keys.ToArray()[i - 1] + ":" + StakeholdersMap[StakeHoldersFromHeader[i - 1]][0]);
+      // 
+    }
+
+  }
 }

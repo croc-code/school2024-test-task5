e8c337098553b37718700deff954af58353a6a1e
TSRSE <26186238+TSRSE@users.noreply.github.com>
2024-05-11 03:48:11 +0300

    [+] Добавил метод для вывода результата в файл Метод принимает в себя макс. значение (для отсеивания стейкхолдеров рейтингом ниже), словарь со всеми стейкхолдерами, путь для записи. Возвращает bool - удалось записать в файл или нет
    [+] Добавил небольшие проверки на существование папки и файлов

--- stakeholders_solution/Program.cs
+++ stakeholders_solution/Program.cs
@@ -10,9 +10,24 @@ internal class Program
     // Stakeholder - [x,y]
     Dictionary<string, double[]> StakeholdersMap = new Dictionary<string, double[]>();
 
-    // 
     string pathToInterestFile = Path.Combine(AppContext.BaseDirectory, @"..\..\..\..\StakeholdersData\interest.txt");
     string pathToInfluenceFile = Path.Combine(AppContext.BaseDirectory, @"..\..\..\..\StakeholdersData\influence.txt");
+    string pathToResultFile = Path.Combine(AppContext.BaseDirectory, @"..\..\..\..\StakeholdersData\result.txt");
+    // 
+    if(!Directory.Exists(Path.Combine(AppContext.BaseDirectory, @"..\..\..\..\StakeholdersData"))){
+      Directory.CreateDirectory(Path.Combine(AppContext.BaseDirectory, @"..\..\..\..\StakeholdersData"));
+
+    }
+
+    // Проверка на наличие нужных файлов
+    if(!File.Exists(pathToInterestFile)){
+      throw new Exception("Файл interest.txt не был найден");
+    }
+
+    if(!File.Exists(pathToInfluenceFile)){
+      throw new Exception("Файл influence.txt не был найден");
+    }
+
 
     StakeholdersMap = await GetUpdatedStakeholdersMapFromFileAsync(StakeholdersMap, pathToInterestFile, 0);
     StakeholdersMap = await GetUpdatedStakeholdersMapFromFileAsync(StakeholdersMap, pathToInfluenceFile, 1);
@@ -20,41 +35,61 @@ internal class Program
     for (int i = 0; i < StakeholdersMap.Keys.ToArray().Length; i++)
     {
       // Debug console
-      Console.WriteLine();
-      Console.WriteLine(StakeholdersMap.Keys.ToArray()[i] + " => " + StakeholdersMap[StakeholdersMap.Keys.ToArray()[i]][0] + " : " + StakeholdersMap[StakeholdersMap.Keys.ToArray()[i]][1]);
+      // Оставил для визуализации отношений Стейкхолдер - координаты
+      Console.WriteLine(StakeholdersMap.Keys.ToArray()[i] + " => { " + StakeholdersMap[StakeholdersMap.Keys.ToArray()[i]][0] + " : " + StakeholdersMap[StakeholdersMap.Keys.ToArray()[i]][1] +" }");
       // 
     }
+
+    if (await WriteMostImportantStakeholdersToFileAsync(StakeholdersMap, pathToResultFile, StakeholdersMap.Keys.ToArray().Length / 2)){
+      Console.Write("Запись в файл results.txt завершена!\nНажмите любую клавишу, чтобы выйти из программы ");
+      Console.ReadKey();
+    }
+
   }
 
+  // Асинхронный метод для считывания данных из 
+  // файла и возвращения нового словаря по завершению выполнения
   private static async Task<Dictionary<string, double[]>> GetUpdatedStakeholdersMapFromFileAsync(Dictionary<string, double[]> currentStakeholdersMap, string path, int coordinateIndex){
     // Читаем асинхронно все линии из файла, 
     // чтобы наше приложение не зависало, пока считывает файл
     string[] LinesFromFile = await File.ReadAllLinesAsync(path);
 
     // Первую линию сплиттим по символу '|', получаем массив стейкхолдеров
-    string[] StakeHoldersFromHeader = LinesFromFile[0].Split('|');
+    string[] StakeholdersFromHeader = LinesFromFile[0].Split('|');
 
     // Добавляем имена стейкхолдеров как значения в наш словарь
-    for (int i = 0; i < StakeHoldersFromHeader.Length; i++)
+    for (int i = 0; i < StakeholdersFromHeader.Length; i++)
     {
-      StakeHoldersFromHeader[i] = StakeHoldersFromHeader[i].TrimStart().TrimEnd();
-      if (!currentStakeholdersMap.ContainsKey(StakeHoldersFromHeader[i]))
+      StakeholdersFromHeader[i] = StakeholdersFromHeader[i].TrimStart().TrimEnd();
+      if (!currentStakeholdersMap.ContainsKey(StakeholdersFromHeader[i]))
       {
-        currentStakeholdersMap.Add(StakeHoldersFromHeader[i], new double[2]);
+        currentStakeholdersMap.Add(StakeholdersFromHeader[i], new double[2]);
       }
     }
 
-    // Присваеваем значение интереса ( X ) к ключу ( Стейкхолдеру )
+    // Присваеваем значение ( influence или interest ) к ключу ( Стейкхолдеру )
     for (int i = 1; i < LinesFromFile.Length; i++)
     {
+      // StakeholdersFromHeader[i - 1] - ключ, получаемый из списка стейкхолдеров, можно было и через *.Keys.ToArray
       double currentLineSumm = LinesFromFile[i].Replace(".", ",").Split(" ").Sum(data => double.TryParse(data, out double number) ? number : 0);
-      currentStakeholdersMap[StakeHoldersFromHeader[i - 1]][coordinateIndex] = currentLineSumm;
-
-      // Debug console
-      Console.WriteLine(LinesFromFile[i]);
-      Console.WriteLine(currentStakeholdersMap.Keys.ToArray()[i - 1] + ":" + currentStakeholdersMap[StakeHoldersFromHeader[i - 1]][coordinateIndex]);
-      // 
+      currentStakeholdersMap[StakeholdersFromHeader[i - 1]][coordinateIndex] = currentLineSumm;
     }
     return currentStakeholdersMap;
+  }
+
+  private static async Task<bool> WriteMostImportantStakeholdersToFileAsync(Dictionary<string, double[]> StakeholdersMap, string path, double minimumScoreAsCoordinate){
+    try
+    {
+      // Производим фильтрацию оп координатам, наши координаты должны быть (не включительно) больше минимально переданного рейтинга (половина количества стейкхолдеров)
+      var FilteredStackholders = StakeholdersMap.Where(stakeholder => stakeholder.Value[0] > minimumScoreAsCoordinate && stakeholder.Value[1] > minimumScoreAsCoordinate).ToDictionary();
+      // Записываем ключи отфильтрованных пользователей в файл
+      await File.WriteAllLinesAsync(path, FilteredStackholders.Keys.ToArray());
+      return true;
+    }
+    catch (Exception ex){
+      Console.WriteLine(@$"Что-то пошло не так! \n{ex.Message}");
+      return false;
+    }
+
   }
 }

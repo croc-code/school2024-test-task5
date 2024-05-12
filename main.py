8ae6ed01660c1d108826a7f85a4c7ab13d9b061c
kudr.max <mdkudryashov@edu.hse.ru>
2024-05-12 14:18:15 +0300

    Готово

--- main.py
+++ main.py
@@ -1,4 +1,9 @@
+# проверить правильность программы
+
 def get_matrix(filename: str):
+    """
+    Функия считывает файл и возвращает матрицу и имена стейкхолдеров
+    """
     with open(filename, 'r') as file:
         lines = file.readlines()
         names = lines[0].strip().split(' | ')
@@ -9,44 +14,46 @@ def get_matrix(filename: str):
 
 
 def calculate_rank(matrix):
+    """
+    Функция расчитывает ранк пользователей по матрице
+
+    ranks[i] — ранк i-го пользователя
+    """
     ranks = [sum(row) for row in matrix]
     return ranks
 
 
-def get_most_valuable_stakeholders(ranks_interest, ranks_influence, names):
-    res = []
-    for i in range(len(ranks_interest)):
-        if ranks_interest[i] > 2.5 and ranks_influence[i] > 2.5:
-            res.append(names[i])
-    return res
+def get_most_valuable_stakeholders():
+    """
+    Функция для нахождения наиболее важных стейкхолдеров
+    """
+    matrix_interest, names_interest = get_matrix('interest.txt')
+    matrix_influence, names_influence = get_matrix('influence.txt')
+
+    ranks_interest = calculate_rank(matrix_interest)
+    ranks_influence = calculate_rank(matrix_influence)
+    
+    is_best_dict = {}
+    for names, ranks in [(names_interest, ranks_interest), (names_influence, ranks_influence)]:
+        for i in range(len(ranks_interest)):
+            name = names[i]
+            rank = ranks[i]
+            if name not in is_best_dict:
+                is_best_dict[name] = True
+            if rank <= 2.5:
+                is_best_dict[name] = False
+
+    best_stakeholders = []
+    for stakeholder_name, is_best in is_best_dict.items():
+        if is_best:
+            best_stakeholders.append(stakeholder_name)
+
+    return best_stakeholders
 
 
-matrix_interest, names_interest = get_matrix('interest.txt')
-matrix_influence, names_influence = get_matrix('influence.txt')
-
-ranks_interest = calculate_rank(matrix_interest)
-ranks_influence = calculate_rank(matrix_influence)
-
-# get_most_valuable_stakeholders(ranks_interest, ranks_influence, names)
-
-# for data, lines in zip([data_interest, data_influence], [lines_interest, lines_influence]):
-#     n = len(lines[0])
-#     for line in lines[1:]:
-#         row = []
-#         for n in line.strip().split():
-#             if n != '_':
-#                 n = float(n)
-#             else:
-#                 n = 0.0
-#             row.append(n)
-#         data.append(row)
-#     data = np.array(data)
-#
-# print(data_interest)
-# print(data_influence)
-#
-# n = len(data_interest)
-#
-# coords_interest = np.empty(n)
-#
-# data_interest[]
+if __name__ == '__main__':
+    best_stakeholders = get_most_valuable_stakeholders()
+    with open('result.txt', 'w') as file:
+        for stakeholder in best_stakeholders:
+            file.write(stakeholder)
+            file.write('\n')
